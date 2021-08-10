2a260d3c970f3f77015e1ce8b05191f56999d65d
connor-debug <70875324+connor-debug@users.noreply.github.com>
2021-08-10 19:04:03 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -7,9 +7,11 @@
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while (0)
 static char *buffer;
-#define METADATA_SIZE 17000000 // 17mb
-//Prob wrong, I need to write a function that takes in a pointer address and maps it to an offset.
-//this will essentially be the locatino in the data structure that it will live.
+#define METADATA_SIZE 131072 // 17 bit offset, 17 + 17 = 34
+#define PAGE_NUM_BITS 8
+//Bounds table is analagous to a page table. Addresses of these BTs are stored in a Bouds directory.
+//We need a bound table Entry for everay possible pointer in the virtual address space
+//We can do it in bigger chunks. Each page (or bounds) covers 4kb of data. 
 
 /* --- PRINTF_BYTE_TO_BINARY macro's --- */
 #define PRINTF_BINARY_PATTERN_INT8 "%c%c%c%c%c%c%c%c"
@@ -36,6 +38,53 @@ static char *buffer;
 #define PRINTF_BYTE_TO_BINARY_INT64(i) \
     PRINTF_BYTE_TO_BINARY_INT32((i) >> 32), PRINTF_BYTE_TO_BINARY_INT32(i)
 /* --- end macros --- */
+
+size_t get_boundt_number(size_t virtual){
+    return (virtual >> PAGE_NUM_BITS);
+}
+
+size_t shift_bits_20(size_t virtual){
+    return (virtual >> 20);
+}
+
+size_t shift_bits_3(size_t virtual){
+    return (virtual >> 3);
+}
+
+
+size_t get_offset_16(size_t virtual){
+    size_t mask = 65535;
+    return virtual & mask;
+}
+
+size_t get_offset(size_t virtual){
+    size_t mask = 255;
+    return virtual & mask;
+}
+
+size_t get_offset_27 (size_t virtual){
+    size_t mask = 134217727;
+    return virtual & mask;
+}
+
+size_t get_bits_20_47(size_t virtual){
+    size_t res = shift_bits_20(virtual);
+    res = get_offset_27(res);
+    return res;
+}
+
+size_t get_bits_3_19(size_t virtual){
+    size_t res = shift_bits_3(virtual);
+    res = get_offset_16(res);
+    return res;
+}
+
+
+size_t get_bounds_location(size_t virtual){
+    size_t bounds = get_offset(virtual) + get_boundt_number(virtual);
+    return bounds;
+}
+
 
 size_t getAddr (void * s){
     size_t int_value = (size_t)s;
@@ -74,9 +123,37 @@ main(int argc, char *argv[])
     printf("Location of the pointer is %p\n", name);
 
     size_t addr = getAddr(name);
+    size_t addr1;
+    size_t addr2;
 
     printf("Location of the derived pointer address is %zu\n", addr);
     printf("This location in binary form is "PRINTF_BINARY_PATTERN_INT64 "\n", PRINTF_BYTE_TO_BINARY_INT64(addr));
+
+    size_t first_chunk = get_bits_20_47(addr);
+    printf("The first chunk is: "PRINTF_BINARY_PATTERN_INT64 "\n", PRINTF_BYTE_TO_BINARY_INT64(first_chunk));
+
+    size_t second_chunk = get_bits_3_19(addr);
+    printf("The second chunk is: "PRINTF_BINARY_PATTERN_INT64 "\n", PRINTF_BYTE_TO_BINARY_INT64(second_chunk));
+
+
+    addr1 = get_offset(addr);
+    printf("the offset (8 bytes ) is %d\n", addr1);
+    printf("binary form of the offset is "PRINTF_BINARY_PATTERN_INT64 "\n", PRINTF_BYTE_TO_BINARY_INT64(addr1));
+
+    size_t bound_page_number = get_boundt_number(addr);
+    printf("The result is %zu\n", bound_page_number);
+    printf("binary form of the boundtable number is "PRINTF_BINARY_PATTERN_INT64 "\n", PRINTF_BYTE_TO_BINARY_INT64(bound_page_number));
+
+    
+    size_t corrected_bits = get_offset_16(bound_page_number);
+    printf("The result is %zu\n", corrected_bits);
+    printf("these are the bits masked with the offset removed "PRINTF_BINARY_PATTERN_INT64 "\n", PRINTF_BYTE_TO_BINARY_INT64(corrected_bits));
+
+    /*size_t boundlocal = get_bounds_location(addr);
+    printf("The result is %zu\n", boundlocal);*/
+
+
+
 
     int err_t = munmap(boundt_ptr, METADATA_SIZE);
     int err_d = munmap(boundd_ptr, METADATA_SIZE);
